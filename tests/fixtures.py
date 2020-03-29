4c08f12439092a369a781b2dfa18250ca19fd0d6
Christoph Rieke <christoph.rieke@up42.com>
2020-03-29 16:23:10 +0200

    reorganize fixtures

--- tests/fixtures.py
+++ tests/fixtures.py
@@ -3,54 +3,46 @@ import os
 import pytest
 import requests_mock
 
-from .context import Auth, Project, Workflow, Job, Tools
-
+from .context import Auth, Project, Workflow, Job, JobTask, Tools
 
 @pytest.fixture()
-def auth_mock():
+def auth_mock_no_request():
     auth = Auth(
         project_id="project_id123",
         project_api_key="project_apikey123",
         authenticate=False,
         retry=False,
+        get_info=False,
     )
-    auth.token = "token_123"
     return auth
 
+@pytest.fixture()
+def auth_mock():
+    # Gets token, but
+    with requests_mock.Mocker() as m:
+        url_token = f"https://project_id123:project_apikey123@api.up42.com/oauth/token"
+        m.post(
+            url=url_token, text='{"data":{"accessToken":"token_789"}}',
+        )
+        auth = Auth(
+            project_id="project_id123",
+            project_api_key="project_apikey123",
+            authenticate=True,
+            retry=False,
+            get_info=True
+        )
+    return auth
+
+@pytest.fixture()
+def auth_live():
+    auth = Auth(
+        project_id=os.getenv("UP42_PROJECT_ID_test_up42_py"),
+        project_api_key=os.getenv("UP42_PROJECT_API_KEY_test_up42_py"),
+    )
+    return auth
 
 @pytest.fixture()
 def project_mock(auth_mock):
-    project = Project(auth=auth_mock, project_id=auth_mock.project_id)
-    return project
-
-
-@pytest.fixture()
-def workflow_mock(auth_mock):
-    workflow = Workflow(
-        auth=auth_mock, workflow_id="workflow_id123", project_id=auth_mock.project_id
-    )
-    return workflow
-
-
-@pytest.fixture()
-def job_mock(auth_mock):
-    job = Job(
-        auth=auth_mock, project_id=auth_mock.project_id, job_id="job_id123"
-    )
-    return job
-
-
-@pytest.fixture()
-def jobtask_mock(auth_mock):
-    pass
-
-
-################################################################
-
-
-@pytest.fixture()
-def project_mock_with_info(auth_mock):
-    auth_mock.authenticate = True
     with requests_mock.Mocker() as m:
         url_project_info = f"{auth_mock._endpoint()}/projects/{auth_mock.project_id}"
         m.get(url=url_project_info, text='{"data": {"xyz":789}, "error":{}}')
@@ -58,15 +50,18 @@ def project_mock_with_info(auth_mock):
         project = Project(auth=auth_mock, project_id=auth_mock.project_id)
     return project
 
+@pytest.fixture()
+def project_live():
+    pass
 
 @pytest.fixture()
-def workflow_mock_with_info(auth_mock):
-    auth_mock.authenticate = True
+def workflow_mock(auth_mock):
+    workflow_id = "workflow_id123"
     with requests_mock.Mocker() as m:
         url_workflow_info = (
-            f"{workflow_mock.auth._endpoint()}/projects/"
-            f"{workflow_mock.project_id}/workflows/"
-            f"{workflow_mock.workflow_id}"
+            f"{auth_mock.auth._endpoint()}/projects/"
+            f"{auth_mock.project_id}/workflows/"
+            f"{workflow_id}"
         )
         m.get(url=url_workflow_info, text='{"data": {"xyz":789}, "error":{}}')
 
@@ -77,27 +72,34 @@ def workflow_mock_with_info(auth_mock):
         )
     return workflow
 
-
 @pytest.fixture()
-def job_mock_with_info(auth_mock):
+def workflow_live():
     pass
 
+# @pytest.fixture()
+# def job_mock_no_info(auth_mock):
+#     return Job(auth=auth_mock, project_id=auth_mock.project_id, job_id="job_id123")
 
 @pytest.fixture()
-def jobtask_mock_with_info(auth_mock):
+def job_mock(auth_mock):
     pass
 
+@pytest.fixture()
+def job_live():
+    pass
 
-################################################################
-
+# @pytest.fixture()
+# def jobtask_mock_no_info(auth_mock):
+#     return JobTask(auth=auth_mock, project_id=auth_mock.project_id, job_id="job_id123", job_task_id="jobtask_id123")
 
 @pytest.fixture()
-def auth_live():
-    auth = Auth(
-        project_id=os.getenv("UP42_PROJECT_ID_test_up42_py"),
-        project_api_key=os.getenv("UP42_PROJECT_API_KEY_test_up42_py"),
-    )
-    return auth
+def jobtask_mock(auth_mock):
+    pass
+
+@pytest.fixture()
+def jobtask_live():
+    pass
+
 
 
 @pytest.fixture()
@@ -110,21 +112,11 @@ def tools_live():
     return tools
 
 
-@pytest.fixture()
-def project_live():
-    pass
 
 
-@pytest.fixture()
-def workflow_live():
-    pass
 
 
-@pytest.fixture()
-def job_live():
-    pass
 
 
-@pytest.fixture()
-def jobtask_live():
-    pass
+
+
