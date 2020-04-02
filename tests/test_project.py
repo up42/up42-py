72897988b0368634fbebb353a6ac9a8e27510642
Christoph Rieke <christoph.rieke@up42.com>
2020-04-02 20:14:19 +0200

    add tests for project module

--- tests/test_project.py
+++ tests/test_project.py
@@ -1,7 +1,14 @@
 import requests_mock
 
 from .context import Project, Workflow
-from .fixtures import auth_mock, project_mock  # pylint: disable=unused-import
+
+# pylint: disable=unused-import
+from .fixtures import (
+    auth_mock,
+    auth_live,
+    project_mock,
+    project_live,
+)
 
 
 def test_project_get_info(project_mock):
@@ -17,10 +24,6 @@ def test_project_get_info(project_mock):
     assert isinstance(project_mock, Project)
     assert info["xyz"] == 789
     assert project_mock.info["xyz"] == 789
-
-
-def test_get_workflows():
-    pass
 
 
 def test_create_workflow(project_mock):
@@ -40,5 +43,82 @@ def test_create_workflow(project_mock):
     assert not hasattr(workflow, "info")
 
 
-def test_create_workflow_use_existing():
-    pass
+def test_create_workflow_use_existing(project_mock):
+    with requests_mock.Mocker() as m:
+        url_workflows_get = f"{project_mock.auth._endpoint()}/projects/{project_mock.project_id}/workflows"
+        m.get(
+            url=url_workflows_get,
+            text='{"data": [{"id":"workflow_id123"}], "error":{}}',
+        )
+
+        url_workflow_info = (
+            f"{project_mock.auth._endpoint()}/projects/"
+            f"{project_mock.project_id}/workflows/workflow_id123"
+        )
+        m.get(
+            url=url_workflow_info,
+            text='{"data": {"name":"workflow_name123", "description":"workflow_description123"}, "error":{}}',
+        )
+
+        workflow = project_mock.create_workflow(
+            name="workflow_name123",
+            description="workflow_description123",
+            use_existing=True,
+        )
+    assert isinstance(workflow, Workflow)
+
+
+def test_get_workflows(project_mock):
+    project_mock.auth.get_info = False
+
+    with requests_mock.Mocker() as m:
+        url_workflows_get = f"{project_mock.auth._endpoint()}/projects/{project_mock.project_id}/workflows"
+        m.get(
+            url=url_workflows_get,
+            text='{"data": [{"id":"workflow_id123"}, {"id":"workflow_id789"}], "error":{}}',
+        )
+
+        workflows = project_mock.get_workflows()
+
+    assert len(workflows) == 2
+    assert isinstance(workflows[0], Workflow)
+
+
+def test_get_workflows_live(project_live):
+    workflows = project_live.get_workflows()
+    assert isinstance(workflows[0], Workflow)
+    assert workflows[0].project_id == project_live.project_id
+
+
+def test_get_project_settings(project_mock):
+    with requests_mock.Mocker() as m:
+        url_project_settings = f"{project_mock.auth._endpoint()}/projects/{project_mock.project_id}/settings"
+        m.get(
+            url=url_project_settings,
+            text='{"data": [{"name":"MAX_CONCURRENT_JOBS"}, {"name":"MAX_AOI_SIZE"}, {"name":"JOB_QUERY_LIMIT_PARAMETER_MAX_VALUE"}], "error":{}}',
+        )
+
+        project_settings = project_mock.get_project_settings()
+    assert isinstance(project_settings, list)
+    assert len(project_settings) == 3
+    assert project_settings[0]["name"] == "MAX_CONCURRENT_JOBS"
+
+
+def test_get_project_settings_live(project_live):
+    project_settings = project_live.get_project_settings()
+    assert isinstance(project_settings, list)
+    assert len(project_settings) == 3
+    assert project_settings[0]["name"] == "MAX_CONCURRENT_JOBS"
+
+
+# def test_update_project_settings(project_mock):
+#     with requests_mock.Mocker() as m:
+#         url_update_project_settings = f"{project_mock.auth._endpoint()}/projects/{project_mock.project_id}/settings"
+#         m.post(url=url_update_project_settings, json=[{'name': 'JOB_QUERY_MAX_AOI_SIZE', 'value': '300'}, {'name': 'MAX_CONCURRENT_JOBS', 'value': '10'}, {'name': 'JOB_QUERY_LIMIT_PARAMETER_MAX_VALUE', 'value': '10'}])
+#
+#         project_mock.update_project_settings(max_concurrent_jobs=300)
+
+
+# TODO: Potentially deactivated by mistake by backend.
+# def test_update_project_settings_live(project_live):
+#     project_live.update_project_settings(max_concurrent_jobs=1)
