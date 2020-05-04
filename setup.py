1d9f177ebaec72ee4369d020babdcea6ef8600d1
Christoph Rieke <christoph.rieke@up42.com>
2020-05-04 14:45:11 +0200

    add verbose license, classifiers, author info to setup py

--- setup.py
+++ setup.py
@@ -3,15 +3,16 @@ from setuptools import setup, find_packages
 
 parent_dir = Path(__file__).resolve().parent
 
-
 setup(
     name="up42-py",
     version=parent_dir.joinpath("up42/_version.txt").read_text(encoding="utf-8"),
+    author="UP42",
+    author_email="support@up42.com",
     description="Python SDK for UP42",
     long_description=parent_dir.joinpath("README.md").read_text(),
     long_description_content_type="text/markdown",
     url="https://github.com/up42/up42-py",
-    license="MIT",
+    license=parent_dir.joinpath("LICENSE").read_text(),
     packages=find_packages(exclude=("tests", "docs", "examples")),
     package_data={
         "": ["_version.txt", "data/aoi_berlin.geojson", "data/aoi_washington.geojson"]
@@ -23,4 +24,11 @@ setup(
         [console_scripts]
         up42=up42.cli:main
     """,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+    ],
 )
