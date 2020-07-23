8786c6e2f0437a985d030e7f6d9b2872f8920f17
Christoph Rieke <christoph.rieke@up42.com>
2020-07-24 00:37:52 +0200

    format

--- up42/tools.py
+++ up42/tools.py
@@ -245,9 +245,11 @@ class Tools:
             if self.results is None:
                 raise ValueError("You first need to download the results!")
             filepaths = self.results
-            # Flatten jobcollection results dictionary.
+            # In case of jobcollection flatten results dictionary.
             if isinstance(filepaths, dict):
-                filepaths = [item for sublist in list(filepaths.values()) for item in sublist]
+                filepaths = [
+                    item for sublist in list(filepaths.values()) for item in sublist
+                ]
 
         plot_file_format = [".tif"]  # TODO: Add other fileformats.
         _plot_images(
