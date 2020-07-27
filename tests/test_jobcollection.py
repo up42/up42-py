3e7a945c6bec6514e3266eb78f05758476d532e0
Rodrigo Almeida <rodrigo.almeida@up42.com>
2020-07-27 17:24:49 +0200

    Add mock test for jobcollection download_result

--- tests/test_jobcollection.py
+++ tests/test_jobcollection.py
@@ -2,6 +2,7 @@ from pathlib import Path
 import tempfile
 
 import pytest
+import requests_mock
 
 # pylint: disable=unused-import,wrong-import-order
 from .fixtures import (
@@ -16,6 +17,30 @@ from .fixtures import (
 
 def test_jobcollection(jobcollection_mock):
     assert len(jobcollection_mock.jobs) == 1
+
+
+def test_jobcollection_download_result(jobcollection_mock):
+    with requests_mock.Mocker() as m:
+        download_url = "http://up42.api.com/abcdef"
+        url_download_result = (
+            f"{jobcollection_mock.auth._endpoint()}/projects/"
+            f"{jobcollection_mock.project_id}/jobs/{jobcollection_mock.jobs_id[0]}/downloads/results/"
+        )
+        m.get(url_download_result, json={"data": {"url": download_url}, "error": {}})
+
+        out_tgz = Path(__file__).resolve().parent / "mock_data/result_tif.tgz"
+        out_tgz_file = open(out_tgz, "rb")
+        m.get(
+            url=download_url,
+            content=out_tgz_file.read(),
+            headers={"x-goog-stored-content-length": "163"},
+        )
+
+        with tempfile.TemporaryDirectory() as tempdir:
+            out_dict = jobcollection_mock.download_results(tempdir)
+            for job_id in out_dict:
+                assert Path(out_dict[job_id][0]).exists()
+                assert len(out_dict[job_id]) == 2
 
 
 @pytest.mark.live
