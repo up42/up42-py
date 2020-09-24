76d4dde5a0bbbc88dfad6fe2f457e2cf04e438f2
Christoph Rieke <christoph.rieke@up42.com>
2020-09-24 23:19:42 +0200

    add mock non200raises test

--- tests/test_auth.py
+++ tests/test_auth.py
@@ -4,6 +4,7 @@ from pathlib import Path
 
 import pytest
 import requests_mock
+import requests
 
 from .context import Auth
 
@@ -110,14 +111,18 @@ def test_request(auth_mock):
     assert response_json == {"data": {"xyz": 789}, "error": {}}
 
 
-# def test_request_raises(auth_mock):
-#     # TODO
-#     pass
-# with requests_mock.Mocker() as m:
-#     m.get(url="http://test.com", text='{"data": {"xyz":789}, "error":{}}')
-#
-#     response_json = auth_mock._request(request_type="GET", url="http://test.com")
-# assert response_json == {"data": {"xyz": 789}, "error": {}}
+def test_request_non200_raises(auth_mock):
+    with requests_mock.Mocker() as m:
+        m.get(
+            url="http://test.com",
+            text='{"data": {}, "error":{"code":403, '
+            '"message": "some 403 error message"}}',
+            status_code=403,
+        )
+
+        with pytest.raises(requests.exceptions.HTTPError) as e:
+            auth_mock._request(request_type="GET", url="http://test.com")
+        assert "some 403 error message!" in str(e.value)
 
 
 def test_request_with_retry(auth_mock):
