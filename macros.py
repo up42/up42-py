7ac92eaa3d6c4c727729403fba1a0040bbcb88ea
Christoph Rieke <christoph.rieke@up42.com>
2021-02-01 14:09:11 +0100

    update gh-pages [ci skip]

---
+++ macros.py
@@ -0,0 +1,133 @@
+"""
+Define snippets and macros for repeated use via mkdocs-macros plugin.
+
+Include variables and macros like regular Python expressions, wrapped in double curly brackets:
+{{ some_variable }} or {{ calculation(x=5) }}
+
+Include external markdown files:
+{% include 'some_folder/some_md_file.md' %}
+
+Changes in macros.py code, for some operations, requires restart of `mkdocs serve`.
+
+Docs: https://mkdocs-macros-plugin.readthedocs.io/en/latest/macros/
+"""
+
+from typing import List, Callable
+import up42
+
+
+def define_env(env):
+    """
+    This is the hook for defining variables, macros and filters.
+
+    Examples:
+            # Use a variable, e.g. {{ some_variable }}
+            env.variables.some_variable = "John Doe"
+
+            # Use a Python function e.g. {{ calculation(x=5) }}
+            @env.macro
+            def calculation(x):
+                    return (2.3 * x) + 7
+
+            # Export some predefined function
+            import math
+            env.macro(math.floor) # will be exported as 'floor'
+    """
+
+    def indent(input_string: str):
+        return input_string.replace("\n", "\n\t")
+
+    def get_methods(
+        c: Callable,
+        exclude: List[str] = None,
+        exclude_tools=True,
+        exclude_viztools=False,
+    ) -> List[str]:
+        """
+        Gets all class methods and properties, excluding specifically excluded ones.
+
+        Args:
+                c: The class object.
+                exclude: Exclude specific methods.
+                exclude_tools: Exclude inherited functions from Tools.
+                exclude_viztools: Exclude inherited functions from VizTools.
+        """
+        property_methods = [
+            name for name, value in vars(c).items() if isinstance(value, property)
+        ]
+        function_methods = [
+            name
+            for name in dir(c)
+            if callable(getattr(c, name))
+            and not name.startswith("_")
+            and not name.startswith("__")
+            and not name[0].isupper()
+        ]
+
+        # TODO: Could also return separatly for separated formatting.
+        function_methods = function_methods + property_methods
+
+        if exclude_tools:
+            function_methods = [f for f in function_methods if f not in dir(up42.Tools)]
+        if exclude_viztools:
+            function_methods = [
+                f for f in function_methods if f not in dir(up42.VizTools)
+            ]
+        if exclude:
+            function_methods = [f for f in function_methods if f not in exclude]
+
+        return function_methods
+
+    # pylint: disable=unused-variable
+    @env.macro
+    def format_funcs(function_methods: List[str]) -> str:
+        """
+        Formats a list of function names to a formatted list for
+        the "Available Functionality" box in the structure chapter.
+        """
+        # Two tabs to stay within the tabbed code box.
+        # Requires Python linebreak and markdown linebreak (two spaces)!
+        # "\n\t\t• `abc`  \n\t\t• `cde`  \n\t\t• `cde`
+
+        formatted = [f"\n\t\t&emsp; • `.{func}()`  " for func in function_methods]
+        return "".join(formatted)
+
+    # Class docstrings variables for use in the structure chapter.
+    # (In code reference added automatically by mkdocstrings).
+    env.variables.docstring_up42 = indent(up42.__doc__)  # init module docstring
+    env.variables.docstring_project = indent(up42.Project.__doc__)
+    env.variables.docstring_workflow = indent(up42.Workflow.__doc__)
+    env.variables.docstring_job = indent(up42.Job.__doc__)
+    env.variables.docstring_jobtask = indent(up42.JobTask.__doc__)
+    env.variables.docstring_jobcollection = indent(up42.JobCollection.__doc__)
+    env.variables.docstring_catalog = indent(up42.Catalog.__doc__)
+    env.variables.docstring_order = indent(up42.Order.__doc__)
+    env.variables.docstring_storage = indent(up42.Storage.__doc__)
+    env.variables.docstring_asset = indent(up42.Asset.__doc__)
+
+    # Class functions for reference and structure chapter
+    env.variables.funcs_up42 = get_methods(
+        up42, exclude_tools=False, exclude_viztools=True
+    )
+    env.variables.funcs_project = get_methods(up42.Project)
+    env.variables.funcs_workflow = get_methods(up42.Workflow)
+    env.variables.funcs_job = get_methods(up42.Job)
+    env.variables.funcs_jobtask = get_methods(
+        up42.JobTask, exclude=["map_quicklooks", "plot_coverage"]
+    )
+    env.variables.funcs_jobcollection = get_methods(
+        up42.JobCollection,
+        exclude=[
+            "get_jobs_info",
+            "get_jobs_status",
+            "plot_quicklooks",
+            "map_quicklooks",
+            "plot_coverage",
+        ],
+    )
+    env.variables.funcs_catalog = get_methods(
+        up42.Catalog, exclude=["plot_results", "map_results"]
+    )
+    env.variables.funcs_order = get_methods(up42.Order)
+    env.variables.funcs_storage = get_methods(up42.Storage)
+    env.variables.funcs_asset = get_methods(up42.Asset)
