c5f0698727396b97414617ce8f1348275050772b
Christoph Rieke <christoph.rieke@up42.com>
2021-11-29 23:36:33 +0100

    repr of specific jobobject instead of picking first of list

--- up42/jobtask.py
+++ up42/jobtask.py
@@ -40,12 +40,11 @@ class JobTask(VizTools):
         self._info = self.info
 
     def __repr__(self):
-        info = self.info[0]
         return (
-            f"JobTask(name: {info['name']}, jobtask_id: {self.jobtask_id}, "
-            f"status: {info['status']}, startedAt: {info['startedAt']}, "
-            f"finishedAt: {info['finishedAt']}, job_name: {info['name']}, "
-            f"block_name: {info['block']['name']}, block_version: {info['blockVersion']}"
+            f"JobTask(name: {self._info['name']}, jobtask_id: {self.jobtask_id}, "
+            f"status: {self._info['status']}, startedAt: {self._info['startedAt']}, "
+            f"finishedAt: {self._info['finishedAt']}, job_name: {self._info['name']}, "
+            f"block_name: {self._info['block']['name']}, block_version: {self._info['blockVersion']}"
         )
 
     @property
