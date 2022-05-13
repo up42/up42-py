4da6387cac85f4e5a78f71b7b3db426e46458f1c
Christoph Rieke <christoph.k.rieke@gmail.com>
2022-05-13 12:52:23 +0200

    add limit and sorting

--- up42/project.py
+++ up42/project.py
@@ -131,7 +131,13 @@ class Project:
             return workflows
 
     def get_jobs(
-        self, return_json: bool = False, test_jobs: bool = True, real_jobs: bool = True
+        self,
+        return_json: bool = False,
+        test_jobs: bool = True,
+        real_jobs: bool = True,
+        limit: int = 500,
+        sortby: str = "createdAt",
+        descending: bool = True,
     ) -> Union[JobCollection, List[dict]]:
         """
         Get all jobs in the project as a JobCollection or json.
@@ -143,25 +149,49 @@ class Project:
             return_json: If true, returns the job info jsons instead of JobCollection.
             test_jobs: Return test jobs or test queries.
             real_jobs: Return real jobs.
+            limit: Optional, only return n first assets by sorting criteria and order.
+                Optimal to select if your workspace contains many assets.
+            sortby: The sorting criteria, one of "createdAt", "name", "id", "mode", "status", "startedAt", "finishedAt".
+            descending: The sorting order, True for descending (default), False for ascending.
 
         Returns:
             All job objects in a JobCollection, or alternatively the jobs info as json.
         """
+        allowed_sorting_criteria = [
+            "createdAt",
+            "name",
+            "id",
+            "mode",
+            "status",
+            "startedAt",
+            "finishedAt",
+        ]
+        if sortby not in allowed_sorting_criteria:
+            raise ValueError(
+                f"sortby parameter must be one of {allowed_sorting_criteria}!"
+            )
+        sort = f"{sortby},{'desc' if descending else 'asc'}"
+
         page = 0
-        url = f"{self.auth._endpoint()}/projects/{self.project_id}/jobs?page={page}"
+        url = f"{self.auth._endpoint()}/projects/{self.project_id}/jobs?page={page}&sort={sort}"
         response_json = self.auth._request(request_type="GET", url=url)
         jobs_json = filter_jobs_on_mode(response_json["data"], test_jobs, real_jobs)
 
         # API get jobs pagination exhaustion is indicated by empty next page (no last page flag)
-        while len(response_json["data"]) > 0:
+        logging.getLogger("up42.utils").setLevel(logging.CRITICAL)
+        while len(response_json["data"]) > 0 and len(jobs_json) < limit:
             page += 1
-            url = f"{self.auth._endpoint()}/projects/{self.project_id}/jobs?page={page}"
+            url = f"{self.auth._endpoint()}/projects/{self.project_id}/jobs?page={page}&sort={sort}"
             response_json = self.auth._request(request_type="GET", url=url)
             if len(response_json["data"]) > 0:
                 jobs_json.extend(
                     filter_jobs_on_mode(response_json["data"], test_jobs, real_jobs)
                 )
-        logger.info(f"Got {len(jobs_json)} jobs in project {self.project_id}.")
+        logging.getLogger("up42.utils").setLevel(logging.INFO)
+        jobs_json = jobs_json[:limit]
+        logger.info(
+            f"Got {len(jobs_json)} jobs (limit parameter {limit}) in project {self.project_id}."
+        )
         if return_json:
             return jobs_json
         else:
