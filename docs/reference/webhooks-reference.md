f24191447a460583ac4569bab380c6d577eabf4e
Christoph Rieke <christoph.k.rieke@gmail.com>
2022-06-09 04:17:59 +0200

    add docs

---
+++ docs/reference/webhooks-reference.md
@@ -0,0 +1,8 @@
+# Webhooks
+
+::: up42.webhooks.Webhooks
+    rendering:
+        show_root_toc_entry: False
+    selection:
+        inherited_members: True
+        members: {{ funcs_storage }}
--- up42/webhooks.py
+++ up42/webhooks.py
@@ -1,128 +1,142 @@
 from typing import List, Optional
 
+from up42.auth import Auth
 from up42.tools import check_auth
+from up42.utils import get_logger
+
+logger = get_logger(__name__)
+# TODO: logger
 
 
-@check_auth
-def get_webhook_events(self) -> dict:
+class Webhooks:
     """
-    Gets all available webhook events.
+    The webhooks class contains UP42 webhooks functionality.
 
-    Returns:
-        A dict of the available webhook events.
+    Use the webhooks:
+    ```python
+    up42.get_webhook_events()
+    ```
     """
-    url = f"{self.auth._endpoint()}/webhook/events"
-    response_json = self.auth._request(request_type="GET", url=url)
-    return response_json["data"]
 
+    def __init__(self, auth: Auth):
+        if auth:
+            self.auth = auth
+            self.workspace_id = auth.workspace_id
 
-@check_auth
-def get_webhooks(self) -> dict:
-    """
-    Gets all registered webhooks for this workspace.
+    def get_webhook_events(self) -> dict:
+        """
+        Gets all available webhook events.
 
-    Returns:
-        A dict of the registered webhooks for this workspace.
-    """
-    url = f"{self.auth._endpoint()}/workspaces/{self.auth.workspace_id}/webhooks"
-    response_json = self.auth._request(request_type="GET", url=url)
-    return response_json["data"]
+        Returns:
+            A dict of the available webhook events.
+        """
+        url = f"{self.auth._endpoint()}/webhook/events"
+        response_json = self.auth._request(request_type="GET", url=url)
+        return response_json["data"]
 
+    @check_auth
+    def get_webhooks(self) -> dict:
+        """
+        Gets all registered webhooks for this workspace.
 
-@check_auth
-def get_webhook(self, webhook_id: str) -> dict:
-    """
-    Gets a specific webhook by its id.
+        Returns:
+            A dict of the registered webhooks for this workspace.
+        """
+        url = f"{self.auth._endpoint()}/workspaces/{self.workspace_id}/webhooks"
+        response_json = self.auth._request(request_type="GET", url=url)
+        return response_json["data"]
 
-    Returns:
-        A dict of the specified webhook in this workspace.
-    """
-    url = f"{self.auth._endpoint()}/workspaces/{self.auth.workspace_id}/webhooks/{webhook_id}"
-    response_json = self.auth._request(request_type="GET", url=url)
+    @check_auth
+    def get_webhook(self, webhook_id: str) -> dict:
+        """
+        Gets a specific webhook by its id.
 
-    return response_json["data"]
+        Returns:
+            A dict of the specified webhook in this workspace.
+        """
+        url = f"{self.auth._endpoint()}/workspaces/{self.workspace_id}/webhooks/{webhook_id}"
+        response_json = self.auth._request(request_type="GET", url=url)
 
+        return response_json["data"]
 
-@check_auth
-def create_webhook(
-    self,
-    name: str,
-    url: str,
-    events: List[str],
-    secret: Optional[str] = None,
-    active: bool = False,
-) -> dict:
-    """
-    Registers a new webhook in the system.
+    @check_auth
+    def create_webhook(
+        self,
+        name: str,
+        url: str,
+        events: List[str],
+        secret: Optional[str] = None,
+        active: bool = False,
+    ) -> dict:
+        """
+        Registers a new webhook in the system.
 
-    Returns:
-        A dict with details of the registered webhook.
-    """
-    input_parameters = {
-        "name": name,
-        url: url,
-        events: events,
-        secret: secret,
-        active: False,
-    }
-    url_post = f"{self.auth._endpoint()}/workspaces/{self.auth.workspace_id}/webhooks"
-    response_json = self.auth._request(
-        request_type="POST", url=url_post, data=input_parameters
-    )
-    return response_json["data"]
+        Returns:
+            A dict with details of the registered webhook.
+        """
+        input_parameters = {
+            "name": name,
+            url: url,
+            events: events,
+            secret: secret,
+            active: False,
+        }
+        url_post = f"{self.auth._endpoint()}/workspaces/{self.workspace_id}/webhooks"
+        response_json = self.auth._request(
+            request_type="POST", url=url_post, data=input_parameters
+        )
+        return response_json["data"]
 
+    @check_auth
+    def update_webhook(
+        self,
+        webhook_id: str,
+        name: str,
+        url: str,
+        events: List[str],
+        secret: Optional[str] = None,
+        active: bool = False,
+    ) -> dict:
+        """
+        Updates a registered webhook.
 
-@check_auth
-def update_webhook(
-    self,
-    webhook_id: str,
-    name: str,
-    url: str,
-    events: List[str],
-    secret: Optional[str] = None,
-    active: bool = False,
-) -> dict:
-    """
-    Updates a registered webhook.
+        Returns:
+            A dict with details of the updated webhook.
+        """
+        input_parameters = {
+            "name": name,
+            url: url,
+            events: events,
+            secret: secret,
+            active: False,
+        }
+        url_post = f"{self.auth._endpoint()}/workspaces/{self.workspace_id}/webhooks/{webhook_id}"
+        response_json = self.auth._request(
+            request_type="POST", url=url_post, data=input_parameters
+        )
+        return response_json["data"]
 
-    Returns:
-        A dict with details of the updated webhook.
-    """
-    input_parameters = {
-        "name": name,
-        url: url,
-        events: events,
-        secret: secret,
-        active: False,
-    }
-    url_post = f"{self.auth._endpoint()}/workspaces/{self.auth.workspace_id}/webhooks/{webhook_id}"
-    response_json = self.auth._request(
-        request_type="POST", url=url_post, data=input_parameters
-    )
-    return response_json["data"]
+    @check_auth
+    def delete_webhook(self, webhook_id: str) -> None:
+        """
+        Deletes a registered webhook.
+        """
+        url = f"{self.auth._endpoint()}/workspaces/{self.workspace_id}/webhooks/{webhook_id}"
+        self.auth._request(request_type="DELETE", url=url)
 
+    @check_auth
+    def trigger_webhook_test_event(self, webhook_id: str) -> dict:
+        """
+        Triggers webhook test event to test your receiving side. The UP42 server will Our server will send test
+        messages for each subscribed event to the specified webhook URL.
 
-@check_auth
-def delete_webhook(self, webhook_id: str) -> None:
-    """
-    Deletes a registered webhook.
-    """
-    url = f"{self.auth._endpoint()}/workspaces/{self.auth.workspace_id}/webhooks/{webhook_id}"
-    self.auth._request(request_type="DELETE", url=url)
-
-
-@check_auth
-def trigger_webhook_test_event(self, webhook_id: str) -> dict:
-    """
-    Triggers webhook test event to test your receiving side. The UP42 server will Our server will send test
-    messages for each subscribed event to the specified webhook URL.
-
-    Returns:
-        A dict with details of the updated webhook.
-    """
-    # TODO: Event names specification
-    url = f"{self.auth._endpoint()}/workspaces/{self.auth.workspace_id}/webhooks/{webhook_id}/tests"
-    response_json = self.auth._request(
-        request_type="POST", url=url, data=input_parameters
-    )
-    return response_json["data"]
+        Returns:
+            A dict with details of the updated webhook.
+        """
+        # TODO: Event names specification
+        url = f"{self.auth._endpoint()}/workspaces/{self.workspace_id}/webhooks/{webhook_id}/tests"
+        response_json = self.auth._request(
+            request_type="POST",
+            url=url,
+        )
+        return response_json["data"]
