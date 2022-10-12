167388fc93c8da1918dd1ba3163259a8118f72a1
Christoph Rieke <christoph.rieke@up42.com>
2022-10-12 20:33:55 +0200

    tasking construct order parameters

--- up42/tasking.py
+++ up42/tasking.py
@@ -2,23 +2,12 @@
 Tasking functionality
 """
 
-from pathlib import Path
-from typing import Union, List, Tuple, Dict, Any
-
-from pandas import Series
-from geopandas import GeoDataFrame
-from shapely.geometry import Polygon
-from geojson import Feature, FeatureCollection
-from tqdm import tqdm
-
 from up42.auth import Auth
-from up42.order import Order
 from up42.catalog import CatalogBase
 from up42.utils import (
     get_logger,
     any_vector_to_fc,
     fc_to_query_geometry,
-    format_time_period,
 )
 
 logger = get_logger(__name__)
@@ -42,58 +31,46 @@ class Tasking(CatalogBase):
     def construct_order_parameters(
         self,
         data_product_id: str,
-        image_id: str,
-        aoi: Union[
-            dict,
-            Feature,
-            FeatureCollection,
-            list,
-            GeoDataFrame,
-            Polygon,
-        ] = None,
+        **kwargs,
     ):
         """
-        Helps constructing the parameters dictionary required for the search.
+        Helps constructing the parameters dictionary required for the tasking order.
 
         Args:
-            data_product_id: Id of the desired UP42 data product, see `catalog.get_data_products`
-            image_id: The id of the desired image (from search results)
-            aoi: The geometry of the order, one of dict, Feature, FeatureCollection,
-                list, GeoDataFrame, Polygon.
+            data_product_id: Id of the desired UP42 data product, see `tasking.get_data_products`
+            kwargs: Any additional required order parameters.
 
         Returns:
             The constructed parameters dictionary.
 
         Example:
             ```python
-            order_parameters = catalog.construct_order_parameters(data_product_id='647780db-5a06-4b61-b525-577a8b68bb54',
-                                                                  image_id='6434e7af-2d41-4ded-a789-fb1b2447ac92',
-                                                                  aoi={'type': 'Polygon',
-                                                                    'coordinates': (((13.375966, 52.515068),
-                                                                      (13.375966, 52.516639),
-                                                                      (13.378314, 52.516639),
-                                                                      (13.378314, 52.515068),
-                                                                      (13.375966, 52.515068)),)})
+            order_parameters = tasking.construct_order_parameters(data_product_id='647780db-5a06-4b61-b525-577a8b68bb54')
             ```
         """
+        order_parameters = {
+            "dataProduct": data_product_id,
+            "params": {**kwargs},
+        }
+        # TODO: geometry handling, additional kwargs
+
         schema = self.get_data_product_schema(data_product_id)
         required_params = list(schema["properties"].keys())
         logger.info(
-            f"This data product requires order_parameters {required_params}. Also see "
-            f".get_data_product_schema()"
+            f"Required order parameters for this data product: {required_params}. Also see "
+            f"tasking.get_data_product_schema()"
         )
+        missing_params = set(required_params).difference(order_parameters["params"])
+        redundant_params = set(order_parameters["params"]).difference(required_params)
 
-        order_parameters = {
-            "dataProduct": data_product_id,
-            "params": {
-                "id": image_id,
-            },
-        }
-        if aoi is not None:
-            aoi = any_vector_to_fc(vector=aoi)
-            aoi = fc_to_query_geometry(fc=aoi, geometry_operation="intersects")
-            order_parameters["params"]["aoi"] = aoi
+        if not missing_params and not redundant_params:
+            logger.info("Correct order parameters!")
+        elif missing_params:
+            logger.info(f"Missing order parameters: {missing_params}")
+        else:
+            logger.info(f"Incorrect order parameters: {redundant_params}")
+
         return order_parameters
 
-        def __repr__(self):
-            return f"Tasking(auth={self.auth})"
+    def __repr__(self):
+        return f"Tasking(auth={self.auth})"
