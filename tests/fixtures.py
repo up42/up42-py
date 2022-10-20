4c93ae2db594086ce8adf95117f65f56416756b8
Christoph Rieke <christoph.rieke@up42.com>
2022-10-20 14:15:00 +0200

    add get data product schema mocks

--- tests/fixtures.py
+++ tests/fixtures.py
@@ -1139,6 +1139,13 @@ def catalog_mock(auth_mock, requests_mock):
         json=json_search_response,
     )
 
+    url_data_product_schema = f"{auth_mock._endpoint()}/orders/schema/{DATA_PRODUCT_ID}"
+    with open(
+        Path(__file__).resolve().parent / "mock_data/data_product_schema.json"
+    ) as json_file:
+        json_data_product_schema = json.load(json_file)
+        requests_mock.get(url=url_data_product_schema, json=json_data_product_schema)
+
     return Catalog(auth=auth_mock)
 
 
