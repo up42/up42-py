7609dca15b47cb20585df01647b38197ae7ac1ed
Christoph Rieke <christoph.rieke@up42.com>
2022-10-21 16:51:22 +0200

    validate collection names via data products call instead

--- up42/catalog.py
+++ up42/catalog.py
@@ -157,6 +157,7 @@ class Catalog(CatalogBase, VizTools):
         self.auth = auth
         self.quicklooks = None
         self.type = "ARCHIVE"
+        self.data_products: Union[dict, None] = None
 
     def __repr__(self):
         return f"Catalog(auth={self.auth})"
@@ -311,11 +312,12 @@ class Catalog(CatalogBase, VizTools):
             search_parameters["limit"] = 500
 
         # UP42 API can query multiple collections of the same host at once.
-        collections = self.get_collections()
+        if self.data_products is None:
+            self.data_products = self.get_data_products(basic=True)
         hosts = [
-            c["hostName"]
-            for c in collections
-            if c["name"] in search_parameters["collections"]
+            v["host"]
+            for v in self.data_products.values()
+            if v["collection"] in search_parameters["collections"]
         ]
         if not hosts:
             raise ValueError(
