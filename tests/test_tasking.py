4301bf64bb5deb47f08a57e259f8ab05c56ebc11
Christoph Rieke <christoph.rieke@up42.com>
2022-10-24 14:02:05 +0200

    fix tasking constructor test

--- tests/test_tasking.py
+++ tests/test_tasking.py
@@ -3,6 +3,7 @@ import json
 
 # pylint: disable=unused-import
 from .fixtures import (
+    auth_mock,
     tasking_mock,
 )
 
@@ -15,8 +16,11 @@ with open(
 def test_construct_order_parameters(tasking_mock):
     order_parameters = tasking_mock.construct_order_parameters(
         data_product_id="data_product_id_123",
-        image_id="123",
-        aoi=mock_search_parameters["intersects"],
+        name="my_tasking_order",
+        acquisition_start="2022-11-01",
+        acquisition_end="2022-11-10",
+        geometry=mock_search_parameters["intersects"],
     )
     assert isinstance(order_parameters, dict)
     assert list(order_parameters.keys()) == ["dataProduct", "params"]
+    assert order_parameters["params"]["acquisitionMode"] is None
