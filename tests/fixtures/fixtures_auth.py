6e5600c242282108691e78f1fc16a3215134c8c3
andher1802 <andresmauricio.hernandezcamacho@gmail.com>
2023-11-09 19:49:28 +0100

    fixing circleci fixture

--- tests/fixtures/fixtures_auth.py
+++ tests/fixtures/fixtures_auth.py
@@ -70,11 +70,8 @@ def project_api_key_live():
 
 
 @pytest.fixture(scope="module")
-def auth_project_live():
-    auth = Auth(
-        project_id=os.getenv("TEST_UP42_PROJECT_ID"),
-        project_api_key=os.getenv("TEST_UP42_PROJECT_API_KEY"),
-    )
+def auth_project_live(project_id_live, project_api_key_live):
+    auth = Auth(project_id=project_id_live, project_api_key=project_api_key_live)
     main._auth = auth  # instead of authenticate()
     return auth
 
