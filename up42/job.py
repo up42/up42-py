1d32e9c6315d592479804cc53a8d65ec276f2ec3
Naman Jain <naman.jain@up42.com>
2023-11-23 10:08:19 +0100

    add deprecation warnings (#497)
    * add deprecation warnings
    
    * fix import
    
    * update deprecation warning
    
    * make flake8 happy
    
    * AT4132-Bugfix get_asset and deprecation logs (#498)
    
    * Deprecating STAC search in get_assets
    
    ---------
    
    Co-authored-by: Daria Lutchenko <102282963+dustgalactic@users.noreply.github.com>
    
    * pre-commit hooks
    
    * black formatting
    
    * add stacklevel to warnings
    
    ---------
    
    Co-authored-by: Andres Hernandez <andresmauricio.hernandezcamacho@gmail.com>
    Co-authored-by: Daria Lutchenko <102282963+dustgalactic@users.noreply.github.com>

--- up42/job.py
+++ up42/job.py
@@ -2,6 +2,7 @@ import logging
 from pathlib import Path
 from time import sleep
 from typing import List, Optional, Union
+from warnings import warn
 
 import requests
 import requests.exceptions
@@ -98,6 +99,12 @@ class Job(VizTools):
         Args:
             report_time: The intervall (in seconds) when to query the job status.
         """
+        warn(
+            "Jobs are getting deprecated. The current analytics platform will be discontinued "
+            "after January 31, 2024, and will be replaced by new processing functionalities.",
+            DeprecationWarning,
+            stacklevel=2,
+        )
         logger.info(
             f"Tracking job status continuously, reporting every {report_time} seconds...",
         )
@@ -129,6 +136,12 @@ class Job(VizTools):
 
     def cancel_job(self) -> None:
         """Cancels a pending or running job."""
+        warn(
+            "Jobs are getting deprecated. The current analytics platform will be discontinued "
+            "after January 31, 2024, and will be replaced by new processing functionalities.",
+            DeprecationWarning,
+            stacklevel=2,
+        )
         url = f"{self.auth._endpoint()}/projects/{self.project_id}/jobs/{self.job_id}/cancel/"
         self.auth._request(request_type="POST", url=url)
         logger.info(f"Job canceled: {self.job_id}")
