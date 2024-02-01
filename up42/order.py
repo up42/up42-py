3255561cb70d2b029e93f8f5251707a9fa82829c
andher1802 <andresmauricio.hernandezcamacho@gmail.com>
2024-02-01 08:50:33 +0100

    returning void dict instead of none in translate to v2 parameters.

--- up42/order.py
+++ up42/order.py
@@ -148,7 +148,7 @@ class Order:
         def translate_construct_parameters(order_parameters):
             feature_collection_template = {"type": "FeatureCollection", "features": []}
             order_parameters_v2 = deepcopy(order_parameters)
-            params = order_parameters_v2.get("params", None)
+            params = order_parameters_v2.get("params", {})
             order_parameters_v2["displayName"] = params.get("id", "Display Name")
             feature = params.pop("aoi", None)
             if feature:
