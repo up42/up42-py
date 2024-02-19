cb0c207ae52d737f5a3dd774f7a2ac16fb34c121
Naman <naman.jain@up42.com>
2024-02-19 11:32:41 +0100

    fix test_tools.py

--- tests/test_tools.py
+++ tests/test_tools.py
@@ -1,6 +1,6 @@
 from pathlib import Path
 
-import geopandas as gpd
+import geopandas as gpd  # type: ignore
 import pytest
 
 from up42.tools import get_example_aoi, read_vector_file
@@ -9,14 +9,14 @@ from up42.tools import get_example_aoi, read_vector_file
 @pytest.mark.parametrize("vector_format", ["geojson", "kml", "wkt"])
 def test_read_vector_file_different_formats(vector_format):
     fp = Path(__file__).resolve().parent / f"mock_data/aoi_berlin.{vector_format}"
-    fc = read_vector_file(filename=fp)
+    fc = read_vector_file(filename=str(fp))
     assert isinstance(fc, dict)
     assert fc["type"] == "FeatureCollection"
 
 
 def test_read_vector_file_as_df():
     fp = Path(__file__).resolve().parent / "mock_data/aoi_berlin.geojson"
-    df = read_vector_file(filename=fp, as_dataframe=True)
+    df = read_vector_file(filename=str(fp), as_dataframe=True)
     assert isinstance(df, gpd.GeoDataFrame)
     assert df.crs.to_epsg() == 4326
 
