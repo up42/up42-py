cd60acbec4aed03fc0e821b5df500bfaff87b6d0
Javid Gafar-zada <javid.gafar-zada@up42.com>
2024-03-20 10:39:13 +0100

    Simplify client test parameterization

--- tests/http/test_client.py
+++ tests/http/test_client.py
@@ -1,4 +1,4 @@
-from typing import Dict, List, Optional
+from typing import Dict, List, Optional, Type
 from unittest import mock
 
 import pytest
@@ -16,16 +16,15 @@ class TestCreate:
     unreachable = mock.MagicMock()
 
     @pytest.mark.parametrize(
-        "project_settings, account_settings, detected_settings",
+        "sources, detected_settings",
         [
-            [EMPTY_PROJECT_CREDENTIALS, ACCOUNT_CREDENTIALS, [None, SETTINGS]],
-            [PROJECT_CREDENTIALS, EMPTY_ACCOUNT_CREDENTIALS, [SETTINGS, None]],
+            [[EMPTY_PROJECT_CREDENTIALS, ACCOUNT_CREDENTIALS], [None, SETTINGS]],
+            [[PROJECT_CREDENTIALS, EMPTY_ACCOUNT_CREDENTIALS], [SETTINGS, None]],
         ],
     )
     def test_should_create_if_only_one_source_is_given(
         self,
-        project_settings: dict,
-        account_settings: dict,
+        sources: List[Dict],
         detected_settings: List[Optional[Dict]],
     ):
         detect_settings = mock.MagicMock(side_effect=detected_settings)
@@ -36,51 +35,33 @@ class TestCreate:
         auth.token.access_token = access_token
         create_auth = mock.MagicMock(return_value=auth)
         result = client.create(
-            [project_settings, account_settings],
+            sources,
             detect_settings=detect_settings,
             detect_retriever=detect_retriever,
             create_auth=create_auth,
         )
         assert result.token == access_token
-        detect_settings.assert_has_calls(
-            [
-                mock.call(project_settings),
-                mock.call(account_settings),
-            ]
-        )
+        detect_settings.assert_has_calls([mock.call(source) for source in sources])
         detect_retriever.assert_called_with(SETTINGS)
         create_auth.assert_called_with(retrieve)
 
-    def test_fails_to_create_if_multiple_sources_are_given(self):
-        detect_settings = mock.MagicMock(return_value=SETTINGS)
-        with pytest.raises(client.MultipleCredentialsSources):
+    @pytest.mark.parametrize(
+        "sources, settings, error",
+        [
+            [[PROJECT_CREDENTIALS, ACCOUNT_CREDENTIALS], SETTINGS, client.MultipleCredentialsSources],
+            [[EMPTY_PROJECT_CREDENTIALS, EMPTY_ACCOUNT_CREDENTIALS], None, client.MissingCredentials],
+        ],
+    )
+    def test_fails_to_create_if_multiple_or_no_sources_are_given(
+        self, sources: List[Dict], settings, error: Type[ValueError]
+    ):
+        detect_settings = mock.MagicMock(return_value=settings)
+        with pytest.raises(error):
             client.create(
-                credential_sources=[PROJECT_CREDENTIALS, ACCOUNT_CREDENTIALS],
+                credential_sources=sources,
                 detect_settings=detect_settings,
                 detect_retriever=self.unreachable,
                 create_auth=self.unreachable,
             )
-        detect_settings.assert_has_calls(
-            [
-                mock.call(PROJECT_CREDENTIALS),
-                mock.call(ACCOUNT_CREDENTIALS),
-            ]
-        )
-        self.unreachable.assert_not_called()
-
-    def test_fails_to_merge_if_no_source_is_given(self):
-        detect_settings = mock.MagicMock(return_value=None)
-        with pytest.raises(client.MissingCredentials):
-            client.create(
-                credential_sources=[EMPTY_PROJECT_CREDENTIALS, EMPTY_ACCOUNT_CREDENTIALS],
-                detect_settings=detect_settings,
-                detect_retriever=self.unreachable,
-                create_auth=self.unreachable,
-            )
-        detect_settings.assert_has_calls(
-            [
-                mock.call(EMPTY_PROJECT_CREDENTIALS),
-                mock.call(EMPTY_ACCOUNT_CREDENTIALS),
-            ]
-        )
+        detect_settings.assert_has_calls([mock.call(source) for source in sources])
         self.unreachable.assert_not_called()
