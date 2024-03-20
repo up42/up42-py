81253a48f25aed7687488c392d8575aacf33ca09
Javid Gafar-zada <javid.gafar-zada@up42.com>
2024-03-20 10:29:36 +0100

    Base client implementation

--- tests/http/test_client.py
+++ tests/http/test_client.py
@@ -5,127 +5,30 @@ import pytest
 
 from up42.http import client
 
-CONFIG_FILE = "some-config"
-SETTINGS_SOURCE = {"some": "settings"}
-CONFIG_CREDENTIALS = {"project_id": "some-id", "password": "some-pass"}
+SETTINGS = {"some": "settings"}
 PROJECT_CREDENTIALS = {"project_id": "some-id", "project_api_key": "some-key"}
 ACCOUNT_CREDENTIALS = {"username": "some-user", "password": "some-pass"}
 EMPTY_PROJECT_CREDENTIALS = {"project_id": None, "project_api_key": None}
 EMPTY_ACCOUNT_CREDENTIALS = {"username": None, "password": None}
 
 
-class TestMergeSettings:
-    @pytest.mark.parametrize(
-        "config, project_settings, account_settings, detected_settings",
-        [
-            [CONFIG_CREDENTIALS, EMPTY_PROJECT_CREDENTIALS, EMPTY_ACCOUNT_CREDENTIALS, [SETTINGS_SOURCE, None, None]],
-            [None, PROJECT_CREDENTIALS, EMPTY_ACCOUNT_CREDENTIALS, [None, SETTINGS_SOURCE, None]],
-            [None, EMPTY_PROJECT_CREDENTIALS, ACCOUNT_CREDENTIALS, [None, None, SETTINGS_SOURCE]],
-        ],
-    )
-    def test_should_merge_if_only_one_source_is_given(
-        self,
-        config: Optional[dict],
-        project_settings: dict,
-        account_settings: dict,
-        detected_settings: List[Optional[Dict]],
-    ):
-        read_config = mock.MagicMock(return_value=config)
-        detect_settings = mock.MagicMock(side_effect=detected_settings)
-        assert (
-            client.merge_settings(
-                CONFIG_FILE,
-                **project_settings,
-                **account_settings,
-                read_config=read_config,
-                detect_settings=detect_settings,
-            )
-            == SETTINGS_SOURCE
-        )
-        read_config.assert_called_with(CONFIG_FILE)
-        detect_settings.assert_has_calls(
-            [
-                mock.call(config or {}),
-                mock.call(project_settings),
-                mock.call(account_settings),
-            ]
-        )
-
-    @pytest.mark.parametrize(
-        "config, project_settings, account_settings, detected_settings",
-        [
-            [
-                CONFIG_CREDENTIALS,
-                PROJECT_CREDENTIALS,
-                EMPTY_ACCOUNT_CREDENTIALS,
-                [SETTINGS_SOURCE, SETTINGS_SOURCE, None],
-            ],
-            [None, PROJECT_CREDENTIALS, ACCOUNT_CREDENTIALS, [None, SETTINGS_SOURCE, SETTINGS_SOURCE]],
-            [
-                CONFIG_CREDENTIALS,
-                EMPTY_PROJECT_CREDENTIALS,
-                ACCOUNT_CREDENTIALS,
-                [SETTINGS_SOURCE, None, SETTINGS_SOURCE],
-            ],
-            [
-                CONFIG_CREDENTIALS,
-                PROJECT_CREDENTIALS,
-                ACCOUNT_CREDENTIALS,
-                [SETTINGS_SOURCE, SETTINGS_SOURCE, SETTINGS_SOURCE],
-            ],
-        ],
-    )
-    def test_fails_to_merge_if_multiple_sources_are_given(
-        self,
-        config: Optional[dict],
-        project_settings: dict,
-        account_settings: dict,
-        detected_settings: List[Optional[Dict]],
-    ):
-        read_config = mock.MagicMock(return_value=config)
-        detect_settings = mock.MagicMock(side_effect=detected_settings)
-        with pytest.raises(client.MultipleCredentialsSources):
-            client.merge_settings(
-                CONFIG_FILE,
-                **project_settings,
-                **account_settings,
-                read_config=read_config,
-                detect_settings=detect_settings,
-            )
-        read_config.assert_called_with(CONFIG_FILE)
-        detect_settings.assert_has_calls(
-            [
-                mock.call(config or {}),
-                mock.call(project_settings),
-                mock.call(account_settings),
-            ]
-        )
-
-    @pytest.mark.parametrize("config", [{}, None])
-    def test_fails_to_merge_if_no_source_is_given(self, config: Optional[dict]):
-        read_config = mock.MagicMock(return_value=config)
-        detect_settings = mock.MagicMock(return_value=None)
-        with pytest.raises(client.MissingCredentials):
-            client.merge_settings(
-                CONFIG_FILE,
-                **EMPTY_PROJECT_CREDENTIALS,
-                **EMPTY_ACCOUNT_CREDENTIALS,
-                read_config=read_config,
-                detect_settings=detect_settings,
-            )
-        read_config.assert_called_with(CONFIG_FILE)
-        detect_settings.assert_has_calls(
-            [
-                mock.call(config or {}),
-                mock.call(EMPTY_PROJECT_CREDENTIALS),
-                mock.call(EMPTY_ACCOUNT_CREDENTIALS),
-            ]
-        )
-
-
 class TestCreate:
-    def test_should_create_client(self):
-        get_settings = mock.MagicMock(return_value=SETTINGS_SOURCE)
+    unreachable = mock.MagicMock()
+
+    @pytest.mark.parametrize(
+        "project_settings, account_settings, detected_settings",
+        [
+            [EMPTY_PROJECT_CREDENTIALS, ACCOUNT_CREDENTIALS, [None, SETTINGS]],
+            [PROJECT_CREDENTIALS, EMPTY_ACCOUNT_CREDENTIALS, [SETTINGS, None]],
+        ],
+    )
+    def test_should_create_if_only_one_source_is_given(
+        self,
+        project_settings: dict,
+        account_settings: dict,
+        detected_settings: List[Optional[Dict]],
+    ):
+        detect_settings = mock.MagicMock(side_effect=detected_settings)
         access_token = "some-token"
         retrieve = mock.sentinel.some_object
         detect_retriever = mock.MagicMock(return_value=retrieve)
@@ -133,13 +36,51 @@ class TestCreate:
         auth.token.access_token = access_token
         create_auth = mock.MagicMock(return_value=auth)
         result = client.create(
-            CONFIG_FILE,
-            **PROJECT_CREDENTIALS,
-            **ACCOUNT_CREDENTIALS,
-            get_settings=get_settings,
+            [project_settings, account_settings],
+            detect_settings=detect_settings,
             detect_retriever=detect_retriever,
             create_auth=create_auth,
         )
         assert result.token == access_token
-        detect_retriever.assert_called_with(SETTINGS_SOURCE)
+        detect_settings.assert_has_calls(
+            [
+                mock.call(project_settings),
+                mock.call(account_settings),
+            ]
+        )
+        detect_retriever.assert_called_with(SETTINGS)
         create_auth.assert_called_with(retrieve)
+
+    def test_fails_to_create_if_multiple_sources_are_given(self):
+        detect_settings = mock.MagicMock(return_value=SETTINGS)
+        with pytest.raises(client.MultipleCredentialsSources):
+            client.create(
+                credential_sources=[PROJECT_CREDENTIALS, ACCOUNT_CREDENTIALS],
+                detect_settings=detect_settings,
+                detect_retriever=self.unreachable,
+                create_auth=self.unreachable,
+            )
+        detect_settings.assert_has_calls(
+            [
+                mock.call(PROJECT_CREDENTIALS),
+                mock.call(ACCOUNT_CREDENTIALS),
+            ]
+        )
+        self.unreachable.assert_not_called()
+
+    def test_fails_to_merge_if_no_source_is_given(self):
+        detect_settings = mock.MagicMock(return_value=None)
+        with pytest.raises(client.MissingCredentials):
+            client.create(
+                credential_sources=[EMPTY_PROJECT_CREDENTIALS, EMPTY_ACCOUNT_CREDENTIALS],
+                detect_settings=detect_settings,
+                detect_retriever=self.unreachable,
+                create_auth=self.unreachable,
+            )
+        detect_settings.assert_has_calls(
+            [
+                mock.call(EMPTY_PROJECT_CREDENTIALS),
+                mock.call(EMPTY_ACCOUNT_CREDENTIALS),
+            ]
+        )
+        self.unreachable.assert_not_called()
--- up42/http/client.py
+++ up42/http/client.py
@@ -1,13 +1,13 @@
 import functools
-import pathlib
-from typing import Optional, Union
+from typing import Callable, Dict, List, Optional
 
-from up42 import utils
 from up42.http import config, oauth
+
+AuthFactory = Callable[[oauth.TokenRetriever], oauth.Up42Auth]
 
 
 class Client:
-    def __init__(self, retrieve: oauth.TokenRetriever, create_auth):
+    def __init__(self, retrieve: oauth.TokenRetriever, create_auth: AuthFactory):
         self.auth = create_auth(retrieve)
 
     @property
@@ -15,50 +15,23 @@ class Client:
         return self.auth.token.access_token
 
 
-Settings = Union[config.AccountCredentialsSettings, config.ProjectCredentialsSettings]
-
-
-def _merge(left: Optional[Settings], right: Optional[Settings]):
+def _merge(left: Optional[config.CredentialsSettings], right: Optional[config.CredentialsSettings]):
     if all([left, right]):
         raise MultipleCredentialsSources("Multiple sources of credentials provided")
     return left or right
 
 
-def merge_settings(
-    cfg_file: Union[str, pathlib.Path, None],
-    project_id: Optional[str],
-    project_api_key: Optional[str],
-    username: Optional[str],
-    password: Optional[str],
-    read_config=utils.read_json,
-    detect_settings=oauth.detect_settings,
-):
-    possible_settings = [
-        detect_settings(settings)
-        for settings in [
-            read_config(cfg_file) or {},
-            {"project_id": project_id, "project_api_key": project_api_key},
-            {"username": username, "password": password},
-        ]
-    ]
-    result = functools.reduce(_merge, possible_settings)
-    if result:
-        return result
-    raise MissingCredentials
-
-
 def create(
-    cfg_file: Union[str, pathlib.Path, None],
-    project_id: Optional[str],
-    project_api_key: Optional[str],
-    username: Optional[str],
-    password: Optional[str],
-    get_settings=merge_settings,
+    credential_sources: List[Dict],
+    detect_settings=oauth.detect_settings,
     detect_retriever=oauth.detect_retriever,
     create_auth=oauth.Up42Auth,
 ):
-    settings = get_settings(cfg_file, project_id, project_api_key, username, password)
-    return Client(detect_retriever(settings), create_auth)
+    possible_settings = [detect_settings(credentials) for credentials in credential_sources]
+    settings = functools.reduce(_merge, possible_settings)
+    if settings:
+        return Client(detect_retriever(settings), create_auth)
+    raise MissingCredentials
 
 
 class MissingCredentials(ValueError):
