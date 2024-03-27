e5ab6a890f088af2b198e2080aba9aeddfa2b04e
Javid Gafar-zada <javid.gafar-zada@up42.com>
2024-03-27 23:08:46 +0100

    Test and bug fix

--- tests/fixtures/fixtures_tasking.py
+++ tests/fixtures/fixtures_tasking.py
@@ -14,7 +14,8 @@ QUOTATION_ENDPOINT = "/v2/tasking/quotation"
 def tasking_mock(auth_mock, requests_mock):
     url_data_product_schema = f"{constants.API_HOST}/orders/schema/{constants.DATA_PRODUCT_ID}"
     with open(
-        pathlib.Path(__file__).resolve().parents[1] / "mock_data/data_product_schema_phr_tasking.json", encoding="utf-8"
+        pathlib.Path(__file__).resolve().parents[1] / "mock_data/data_product_schema_phr_tasking.json",
+        encoding="utf-8",
     ) as json_file:
         json_data_product_schema = json.load(json_file)
         requests_mock.get(url=url_data_product_schema, json=json_data_product_schema)
@@ -81,7 +82,11 @@ def tasking_mock(auth_mock, requests_mock):
             json=json_data_get_quotation,
         )
 
-    wrong_id_response_json = json.dumps({"status": 404, "title": "Resource does not exist.", "detail": {}})
+    wrong_id_response_json = {
+        "status": 404,
+        "title": "Resource does not exist.",
+        "detail": {},
+    }
     decide_quotation_endpoint = f"/v2/tasking/quotation/{constants.QUOTATION_ID}-01"
     url_decide_quotation_fail = f"{constants.API_HOST}{decide_quotation_endpoint}"
     requests_mock.patch(
@@ -92,13 +97,12 @@ def tasking_mock(auth_mock, requests_mock):
 
     decide_quotation_endpoint = f"/v2/tasking/quotation/{constants.QUOTATION_ID}-02"
     url_decide_quotation_accepted = f"{constants.API_HOST}{decide_quotation_endpoint}"
-    accepted_id_response_json = json.dumps(
-        {
-            "status": 405,
-            "title": "Resource (Quotation) is write-protected.",
-            "detail": {},
-        }
-    )
+    accepted_id_response_json = {
+        "status": 405,
+        "title": "Resource (Quotation) is write-protected.",
+        "detail": {},
+    }
+
     requests_mock.patch(
         url=url_decide_quotation_accepted,
         status_code=405,
