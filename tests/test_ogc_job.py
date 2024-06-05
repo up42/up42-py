d9c66badc09887f3ac0e71f572f09082a33d8cc3
andher1802 <andresmauricio.hernandezcamacho@gmail.com>
2024-06-05 22:27:16 +0200

    adjust Changelog

---
+++ tests/test_ogc_job.py
@@ -0,0 +1,13 @@
+from unittest import mock
+
+import pytest
+import requests
+
+
+@pytest.fixture(autouse=True)
+def workspace():
+    with mock.patch("up42.base.workspace") as workspace_mock:
+        session = requests.Session()
+        session.hooks = {"response": lambda response, *args, **kwargs: response.raise_for_status()}
+        workspace_mock.auth.session = session
+        yield
