aba4c27b5017c1883584e9bb4e75bb6d61df97eb
Javid Gafar-zada <javid.gafar-zada@up42.com>
2024-06-11 09:15:19 +0200

    [PT-5055] Job template execution (#628)
    * Job template execution
    
    * Adapt changes from parent
    
    * Apply hooks

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -29,6 +29,12 @@ You can check your current version with the following command:
     ```
 
 For more information, see [UP42 Python package description](https://pypi.org/project/up42-py/).
+## 1.0.4a15
+
+**Jun 11, 2024**
+
+- Added job template execution to `templates.py`
+
 # 1.0.4a14
 
 **Jun 10, 2024**
--- up42/processing.py
+++ up42/processing.py
@@ -16,6 +16,72 @@ class ValidationError:
     name: str
 
 
+class JobStatus(enum.Enum):
+    CREATED = "created"
+    VALID = "valid"
+    INVALID = "invalid"
+    ACCEPTED = "accepted"
+    REJECTED = "rejected"
+    RUNNING = "running"
+    SUCCESSFUL = "successful"
+    FAILED = "failed"
+    CAPTURED = "captured"
+    RELEASED = "released"
+
+
+class JobMetadata(TypedDict):
+    processID: str  # pylint: disable=invalid-name
+    jobID: str  # pylint: disable=invalid-name
+    accountID: str  # pylint: disable=invalid-name
+    workspaceID: Optional[str]  # pylint: disable=invalid-name
+    definition: dict
+    status: str
+    created: str
+    started: Optional[str]
+    finished: Optional[str]
+    updated: str
+
+
+@dataclasses.dataclass
+class Job:
+    session = base.Session()
+    process_id: str
+    id: str
+    account_id: str
+    workspace_id: Optional[str]
+    definition: dict
+    status: JobStatus
+    created: datetime.datetime
+    updated: datetime.datetime
+    started: Optional[datetime.datetime] = None
+    finished: Optional[datetime.datetime] = None
+
+    @staticmethod
+    def __to_datetime(value: Optional[str]):
+        return value and datetime.datetime.fromisoformat(value.rstrip("Z"))
+
+    @staticmethod
+    def from_metadata(metadata: JobMetadata) -> "Job":
+        return Job(
+            process_id=metadata["processID"],
+            id=metadata["jobID"],
+            account_id=metadata["accountID"],
+            workspace_id=metadata["workspaceID"],
+            definition=metadata["definition"],
+            status=JobStatus(metadata["status"]),
+            created=Job.__to_datetime(metadata["created"]),
+            started=Job.__to_datetime(metadata["started"]),
+            finished=Job.__to_datetime(metadata["finished"]),
+            updated=Job.__to_datetime(metadata["updated"]),
+        )
+
+    @classmethod
+    def get(cls, job_id: str) -> "Job":
+        url = host.endpoint(f"/v2/processing/jobs/{job_id}")
+        metadata = cls.session.get(url).json()
+        return cls.from_metadata(metadata)
+
+
 CostType = Union[int, float, "Cost"]
 
 
@@ -94,7 +160,10 @@ class JobTemplate:
     def is_valid(self) -> bool:
         return not self.errors
 
-    # TODO: def create_job(self) -> Job:
+    def execute(self) -> Job:
+        url = host.endpoint(f"/v2/processing/processes/{self.process_id}/execution")
+        job_metadata = self.session.post(url, json={"inputs": self.inputs}).json()
+        return Job.from_metadata(job_metadata)
 
 
 @dataclasses.dataclass
@@ -118,69 +187,3 @@ class MultiItemJobTemplate(JobTemplate):
             "title": self.title,
             "items": [item.get_self_href() for item in self.items],
         }
-
-
-class JobStatus(enum.Enum):
-    CREATED = "created"
-    VALID = "valid"
-    INVALID = "invalid"
-    ACCEPTED = "accepted"
-    REJECTED = "rejected"
-    RUNNING = "running"
-    SUCCESSFUL = "successful"
-    FAILED = "failed"
-    CAPTURED = "captured"
-    RELEASED = "released"
-
-
-class JobMetadata(TypedDict):
-    processID: str  # pylint: disable=invalid-name
-    jobID: str  # pylint: disable=invalid-name
-    accountID: str  # pylint: disable=invalid-name
-    workspaceID: Optional[str]  # pylint: disable=invalid-name
-    definition: dict
-    status: str
-    created: str
-    started: Optional[str]
-    finished: Optional[str]
-    updated: str
-
-
-@dataclasses.dataclass
-class Job:
-    session = base.Session()
-    process_id: str
-    id: str
-    account_id: str
-    workspace_id: Optional[str]
-    definition: dict
-    status: JobStatus
-    created: datetime.datetime
-    updated: datetime.datetime
-    started: Optional[datetime.datetime] = None
-    finished: Optional[datetime.datetime] = None
-
-    @staticmethod
-    def __to_datetime(value: Optional[str]):
-        return value and datetime.datetime.fromisoformat(value.rstrip("Z"))
-
-    @staticmethod
-    def from_metadata(metadata: JobMetadata) -> "Job":
-        return Job(
-            process_id=metadata["processID"],
-            id=metadata["jobID"],
-            account_id=metadata["accountID"],
-            workspace_id=metadata["workspaceID"],
-            definition=metadata["definition"],
-            status=JobStatus(metadata["status"]),
-            created=Job.__to_datetime(metadata["created"]),
-            started=Job.__to_datetime(metadata["started"]),
-            finished=Job.__to_datetime(metadata["finished"]),
-            updated=Job.__to_datetime(metadata["updated"]),
-        )
-
-    @classmethod
-    def get(cls, job_id: str) -> "Job":
-        url = host.endpoint(f"/v2/processing/jobs/{job_id}")
-        metadata = cls.session.get(url).json()
-        return cls.from_metadata(metadata)
