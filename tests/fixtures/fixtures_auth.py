6c0a5a19eface00227a8d5669c57415e6e88e67f
Javid Gafar-zada <javid.gafar-zada@up42.com>
2024-06-27 11:44:03 +0200

    Changelog and version update

--- tests/fixtures/fixtures_auth.py
+++ tests/fixtures/fixtures_auth.py
@@ -2,7 +2,6 @@ import pytest
 import requests_mock as req_mock
 
 from up42 import auth as up42_auth
-
 from . import fixtures_globals as constants
 
 
@@ -11,7 +10,8 @@ def auth_mock(requests_mock: req_mock.Mocker) -> up42_auth.Auth:
     token_payload = {
         "data": {"accessToken": constants.TOKEN},
         "access_token": constants.TOKEN,
+        "expires_in": 5 * 60,
         "token_type": "bearer",
     }
-    requests_mock.post("https://api.up42.com/oauth/token", json=token_payload)
+    requests_mock.post(constants.TOKEN_ENDPOINT, json=token_payload)
     return up42_auth.Auth(username=constants.USER_EMAIL, password=constants.PASSWORD)
