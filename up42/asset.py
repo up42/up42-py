21c687047d1fc0a1efcde00aad66c8bc4d486d64
andher1802 <andresmauricio.hernandezcamacho@gmail.com>
2024-10-16 19:17:42 +0200

    Fix Asset_id property type

--- up42/asset.py
+++ up42/asset.py
@@ -50,7 +50,7 @@ class Asset:
         return self.info.__repr__()
 
     @property
-    def asset_id(self) -> dict:
+    def asset_id(self) -> str:
         return self.info.get("id")
 
     def _get_info(self, asset_id: str):
