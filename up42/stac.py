9e2bddb4ad1e0724dad906df99213b7b47737b64
Javid Gafar-zada <javid.gafar-zada@up42.com>
2024-12-19 17:19:01 +0100

    FileProvider descriptor

---
+++ up42/stac.py
@@ -0,0 +1,32 @@
+from typing import Optional
+
+import pystac
+
+from up42 import base, utils
+
+
+class InvalidUp42Asset(ValueError):
+    pass
+
+
+class FileProvider:
+    session = base.Session()
+
+    def __get__(
+        self, obj: Optional[pystac.Asset], obj_type=None
+    ) -> Optional[utils.ImageFile]:
+        if obj:
+            if obj.href.startswith("https://api.up42"):
+                url = obj.href + "/download-url"
+                signed_url = self.session.post(url=url).json()["url"]
+                return utils.ImageFile(
+                    url=signed_url, file_name=obj.title, session=self.session
+                )
+            else:
+                return None
+        else:
+            raise AttributeError
+
+
+def extend():
+    pystac.Asset.file = FileProvider()
