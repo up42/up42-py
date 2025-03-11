fa309b4dc08612df00da08040b1922308834466c
Paige Amelie Quint <paige.quint@up42.com>
2025-03-11 13:00:46 +0100

    feat(USER-2337): cleanup

--- tests/test_host.py
+++ tests/test_host.py
@@ -2,8 +2,8 @@ import pytest
 
 from up42 import host
 
-ENDPOINT = "https://api.up42.com/some-path"
-SA_ENDPOINT = "https://api.sa.up42.com/some-path"
+ENDPOINT = "https://api.up42.com/path"
+SA_ENDPOINT = "https://api.sa.up42.com/path"
 TOKEN_ENDPOINT = "https://auth.up42.com/realms/public/protocol/openid-connect/token"
 SA_TOKEN_ENDPOINT = "https://auth.sa.up42.com/realms/public/protocol/openid-connect/token"
 USER_INFO_ENDPOINT = "https://auth.up42.com/realms/public/protocol/openid-connect/userinfo"
@@ -47,13 +47,13 @@ class TestHost:
     )
     def test_should_use_correct_endpoint_url_for_requested_region(self, region, expected):
         host.REGION = region
-        assert expected == host.endpoint("/some-path")
+        assert expected == host.endpoint("/path")
 
     @pytest.mark.parametrize("region", ["xyz", "abc"])
     def test_should_raise_an_error_when_retrieving_endpoint_for_unknown_region(self, region):
         host.REGION = region
         with pytest.raises(host.UnsupportedRegion):
-            host.endpoint("/some-path")
+            host.endpoint("/path")
 
     @pytest.mark.parametrize(
         "region, expected",
