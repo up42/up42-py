e346e908e7dda365a735d68318e4cb35b215d7ad
Matheus Pinheiro <matheus.pinheiro@up42.com>
2025-08-15 14:27:46 +0200

    Fix how collection items are retrieved in BulkDeletion

--- up42/stac.py
+++ up42/stac.py
@@ -113,23 +113,23 @@ class BulkDeletion:
 
     def delete(self):
         items = self.stac_client.get_items(*self._item_ids)
-        collections: set[pystac.Collection] = set()
+        collection_ids = set()
         for item in items:
-            collection = item.get_parent()
-            collections.add(collection)  # type: ignore
+            collection_ids.add(item.collection_id)
 
-        for collection in collections:
-            collection_item_ids = {item_in_collection.id for item_in_collection in collection.get_items()}
+        for collection_id in collection_ids:
+            collection_items = self.stac_client.get_collection(collection_id).get_items()
+            collection_item_ids = {item_in_collection.id for item_in_collection in collection_items}
             missing_items = collection_item_ids - self._item_ids
             if missing_items:
                 error_msg = (
                     f"The deletion request failed because the submitted items are part of a group that must be deleted "
-                    f"together. The submitted items belong to the following collection: '{collection.id}'. All items "
+                    f"together. The submitted items belong to the following collection: '{collection_id}'. All items "
                     f"from this collection must be deleted at once. To proceed, please add these missing item IDs "
                     f"to your request: {list(missing_items)}."
                 )
                 raise IncompleteCollectionDeletionError(error_msg)
 
-        for collection in collections:
-            url = host.endpoint(f"/v2/assets/stac/collections/{collection.id}")
+        for collection_id in collection_ids:
+            url = host.endpoint(f"/v2/assets/stac/collections/{collection_id}")
             self.session.delete(url=url)
