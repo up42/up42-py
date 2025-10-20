b355bcd2874a8e671e3072fd30da825b2aee090f
JeremiahDo <jeremiah.dominguez-gorrin@up42.com>
2025-10-20 16:09:00 +0200

    fix test

--- tests/test_storage.py
+++ tests/test_storage.py
@@ -1,4 +1,3 @@
-import datetime as dt
 import random
 from typing import Optional
 
@@ -21,52 +20,6 @@ def mock_items(get_items: mock.MagicMock, return_json: bool):
 
 
 class TestStorage:
-    @pytest.mark.parametrize("limit", [10, None])
-    @pytest.mark.parametrize("descending", [False, True])
-    @pytest.mark.parametrize("return_json", [False, True])
-    def test_should_get_assets(
-        self,
-        limit: Optional[int],
-        descending: bool,
-        return_json: bool,
-    ):
-        created_after = dt.datetime.now() - dt.timedelta(days=1)
-        created_before = dt.datetime.now() + dt.timedelta(days=1)
-        collection_names = ["collection", "names"]
-        producer_names = ["producer", "names"]
-        tags = ["producer", "names"]
-        sources = ["some", "sources"]
-        search = "search"
-        sortby = "updatedAt"
-        with mock.patch("up42.asset.Asset.all") as get_assets:
-            mock_items(get_assets, return_json)
-            assets = storage.Storage().get_assets(
-                created_after=created_after,
-                created_before=created_before,
-                workspace_id=constants.WORKSPACE_ID,
-                collection_names=collection_names,
-                producer_names=producer_names,
-                tags=tags,
-                sources=sources,
-                search=search,
-                limit=limit,
-                sortby=sortby,
-                descending=descending,
-                return_json=return_json,
-            )
-            assert assets == SENTINELS[:limit]
-            get_assets.assert_called_with(
-                created_after=created_after,
-                created_before=created_before,
-                workspace_id=constants.WORKSPACE_ID,
-                collection_names=collection_names,
-                producer_names=producer_names,
-                tags=tags,
-                sources=sources,
-                search=search,
-                sort_by=utils.SortingField(sortby, not descending),
-            )
-
     @pytest.mark.parametrize("limit", [10, None])
     @pytest.mark.parametrize("descending", [False, True])
     @pytest.mark.parametrize("workspace_orders", [False, True])
