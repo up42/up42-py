17d7a03f544ed3482172b1123ccff12fb201a4b2
mohamed-aboshosha-up42 <mohamed.a.aboshosha@up42.com>
2026-01-29 13:09:42 +0100

    Moving TestSingleItemJobTemplate andTestMultiItemJobTemplate to test_processing_templates.py.

--- tests/test_processing_templates.py
+++ tests/test_processing_templates.py
@@ -1,12 +1,15 @@
 import dataclasses
 import random
+import uuid
 from typing import Optional
 from unittest import mock
 
 import pystac
 import pytest
+import requests
+import requests_mock as req_mock
 
-from tests import constants
+from tests import constants, helpers
 from up42 import processing
 from up42 import processing_templates as templates
 
@@ -19,6 +22,33 @@ second_item = mock.MagicMock(spec=pystac.Item)
 item.get_self_href.return_value = ITEM_URL
 second_item.get_self_href.return_value = SECOND_ITEM_URL
 PROCESS_ID = "process-id"
+EULA_ID = str(uuid.uuid4())
+PROCESS_URL = f"{constants.API_HOST}/v2/processing/processes/{PROCESS_ID}"
+PROCESS_SUMMARY = {
+    "id": PROCESS_ID,
+    "additionalParameters": {
+        "parameters": [
+            {"name": "eula-id", "value": [EULA_ID]},
+            {"name": "price", "value": [{"credits": 1, "unit": "SQ_KM"}]},
+        ]
+    },
+}
+EULA_URL = f"{constants.API_HOST}/v2/eulas/{EULA_ID}"
+VALIDATION_URL = f"{constants.API_HOST}/v2/processing/processes/{PROCESS_ID}/validation"
+COST_URL = f"{constants.API_HOST}/v2/processing/processes/{PROCESS_ID}/cost"
+ITEM = pystac.Item.from_dict(
+    {
+        "type": "Feature",
+        "stac_version": "1.0.0",
+        "id": "id",
+        "properties": {"datetime": "2024-01-01T00:00:00.000000Z"},
+        "geometry": {"type": "Point", "coordinates": (0, 0)},
+        "links": [{"rel": "self", "href": ITEM_URL}],
+        "assets": {},
+        "bbox": [0, 0, 0, 0],
+        "stac_extensions": [],
+    }
+)
 
 
 @pytest.fixture(autouse=True)
@@ -29,6 +59,10 @@ def template_post_init():
     with mock.patch.object(templates.JobTemplate, "__post_init__", initialize):
         yield
 
+@pytest.fixture
+def process_found_and_eula_accepted(requests_mock: req_mock.Mocker):
+    requests_mock.get(PROCESS_URL, json=PROCESS_SUMMARY)
+    requests_mock.get(EULA_URL, json={"isAccepted": True})
 
 class TestParameterlessTemplates:
     @pytest.mark.parametrize(
@@ -102,19 +136,37 @@ class TestSimularityProcesses:
         }
 
 
+
+
 @dataclasses.dataclass
 class SampleMultiItemJobTemplate(templates.MultiItemJobTemplate):
     process_id = PROCESS_ID
 
 
 class TestMultiItemJobTemplate:
-    def test_should_provide_inputs(self):
+    @pytest.mark.usefixtures("process_found_and_eula_accepted")
+    def test_should_provide_inputs(self, requests_mock: req_mock.Mocker):
+        cost = processing.Cost(strategy="discount", credits=-1)
+        body_matcher = helpers.match_request_body({"inputs": {"title": TITLE, "items": [ITEM_URL]}})
+        requests_mock.post(
+            VALIDATION_URL,
+            status_code=200,
+            additional_matcher=body_matcher,
+        )
+        requests_mock.post(
+            COST_URL,
+            status_code=200,
+            json={"pricingStrategy": cost.strategy, "totalCredits": cost.credits},
+            additional_matcher=body_matcher,
+        )
         template = SampleMultiItemJobTemplate(
-            items=[item],
+            items=[ITEM],
             title=TITLE,
         )
-        assert template.is_valid and template.cost == COST
+        assert template.is_valid
+        assert template.cost == cost
         assert template.inputs == {"title": TITLE, "items": [ITEM_URL]}
+
 
 
 @dataclasses.dataclass
@@ -123,11 +175,25 @@ class SampleSingleItemJobTemplate(templates.SingleItemJobTemplate):
 
 
 class TestSingleItemJobTemplate:
-    def test_should_provide_inputs(self):
+    @pytest.mark.usefixtures("process_found_and_eula_accepted")
+    def test_should_provide_inputs(self, requests_mock: req_mock.Mocker):
+        cost = processing.Cost(strategy="discount", credits=-1)
+        body_matcher = helpers.match_request_body({"inputs": {"title": TITLE, "item": ITEM_URL}})
+        requests_mock.post(
+            VALIDATION_URL,
+            status_code=200,
+            additional_matcher=body_matcher,
+        )
+        requests_mock.post(
+            COST_URL,
+            status_code=200,
+            json={"pricingStrategy": cost.strategy, "totalCredits": cost.credits},
+            additional_matcher=body_matcher,
+        )
         template = SampleSingleItemJobTemplate(
-            item=item,
+            item=ITEM,
             title=TITLE,
         )
         assert template.is_valid
-        assert template.cost == COST
+        assert template.cost == cost
         assert template.inputs == {"title": TITLE, "item": ITEM_URL}
